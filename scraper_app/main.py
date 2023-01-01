8a7d63af9f9b631dca91dd41ee90aaade49af385
realdragonturtle <29815169+realdragonturtle@users.noreply.github.com>
2023-01-01 16:39:45 +0100

    add health endpoints

--- scraper_app/main.py
+++ scraper_app/main.py
@@ -1,6 +1,7 @@
 from typing import List
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status, Response
 from . import schemas, utility
+import time 
 
 
 description = "Service for getting prices"
@@ -8,11 +9,15 @@ tags_metadata = [
     {
         "name": "prices",
         "description": "Operations with price data"
+    },
+    {
+        "name": "health",
+        "description": "Operations checking service health status"
     }
 ]
 
 
-app = FastAPI(title="Price scraper", description=description, openapi_tags=tags_metadata)
+app = FastAPI(title="Price scraper", description=description, openapi_tags=tags_metadata, docs_url="/openapi")
 
 
 @app.post("/prices/", response_model=List[schemas.Price], tags=["prices"])
@@ -30,3 +35,21 @@ def get_prices():
     """
     prices = utility.get_all_prices2()
     return prices
+
+
+@app.get("/health/liveness", status_code=status.HTTP_200_OK, tags=["health"])
+def get_liveness():
+    """
+    Checks liveness
+    """
+
+@app.get("/health/readiness", status_code=status.HTTP_200_OK, tags=["health"])
+def get_readiness():
+    """
+    Checks readiness
+    """
+    
+    
+
+
+    
